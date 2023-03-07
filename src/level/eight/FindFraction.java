a78575c4f1f6fbf6808203292c6ebf07b9b1fe54
seo-rio <scs9310@gmail.com>
2023-03-08 02:09:16 +0900

    백준 8단계 2문제 완료

---
+++ src/level/eight/FindFraction.java
@@ -0,0 +1,34 @@
+package level.eight;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @분수_찾기 https://www.acmicpc.net/problem/1193
+ */
+public class FindFraction {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+
+        int index = 1;
+        int count = 1;
+
+        while (index < x) {
+            count++;
+            index += count;
+        }
+
+        if (count % 2 == 0) {
+            System.out.println((count - (index - x)) + "/" + (index - x + 1));
+        } else {
+            System.out.println((index - x + 1) + "/" + (count - (index - x)));
+
+        }
+
+
+    }
+}
