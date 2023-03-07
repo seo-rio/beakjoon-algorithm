fa3559fc91b1485768373f31f2c1dd461212d1ba
seo-rio <scs9310@gmail.com>
2023-03-07 18:51:30 +0900

    백준 7단계 3문제 완료

---
+++ src/level/seven/Max.java
@@ -0,0 +1,36 @@
+package level.seven;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * @최댓값 https://www.acmicpc.net/problem/2566
+ */
+public class Max {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk;
+
+        int maxValue = 0;
+        int maxPosX = 0;
+        int maxPosY = 0;
+        for (int i = 0; i < 9; i++) {
+            stk = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < 9; j++) {
+                int num = Integer.parseInt(stk.nextToken());
+                if(maxValue < num) {
+                    maxValue = num;
+                    maxPosX = i;
+                    maxPosY = j;
+                }
+
+            }
+        }
+        System.out.println(maxValue);
+        System.out.println(++maxPosX + " " + ++maxPosY);
+    }
+}
