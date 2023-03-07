dbed737f359c7003ca05d053c93457c8cccde104
seo-rio <scs9310@gmail.com>
2023-03-07 15:40:44 +0900

    백준 6단계 전체 완료

---
+++ src/level/six/GroupWordChecker.java
@@ -0,0 +1,51 @@
+package level.six;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * @그룹_단어_체커 https://www.acmicpc.net/problem/1316
+ */
+public class GroupWordChecker {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int wordCount = Integer.parseInt(stk.nextToken());
+
+        int count = wordCount;
+        for (int i = 0; i < wordCount; i++) {
+            stk = new StringTokenizer(br.readLine());
+            String word = stk.nextToken();
+
+            for (int j = 0; j < word.length(); j++) {
+                char alphabet = word.charAt(j);
+                int start = word.indexOf(alphabet);
+                int end = word.lastIndexOf(alphabet);
+                boolean isGroup = true;
+
+                for (int k = start + 1; k < end; k++) {
+                    if(word.charAt(k) != alphabet) {
+                        isGroup = false;
+                        break;
+                    }
+                }
+
+                if(!isGroup) {
+                    count--;
+                    break;
+                }
+
+            }
+
+        }
+        System.out.println("count => " + count);
+    }
+
+
+}
