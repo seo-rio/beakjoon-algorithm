608d0eb16e08d3f5eff072a13cf0a8b4d0be438b
seo-rio <scs9310@gmail.com>
2023-03-13 15:02:00 +0900

    백준 9단계 전체 완료

--- src/level/nine/Goldbach.java
+++ src/level/nine/Goldbach.java
@@ -3,9 +3,6 @@ package level.nine;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 /**
  * @골드바흐의_추측 https://www.acmicpc.net/problem/9020
@@ -14,60 +11,53 @@ public class Goldbach {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stk = new StringTokenizer(br.readLine());
+//        StringTokenizer stk = new StringTokenizer(br.readLine());
 
-        int testCase = Integer.parseInt(stk.nextToken());
+        int testCase = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < testCase; i++) {
-            stk = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(stk.nextToken());
+//            stk = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(br.readLine());
 
             int halfNum = num / 2;
 
-            for (int j = 1; j <= halfNum; j++) {
-                int result = num - j;
-                System.out.println(num + " - " + j + " = " + result);
+            int num1 = 0;
+            int num2 = 0;
 
-//                System.out.println(result);
+            for (int j = 2; j <= halfNum; j++) {
+                int result = num - j;
+
+                int jCount = 0;
+                int rCount = 0;
+
+                System.out.println("j => " + j);
+                System.out.println("(int) Math.sqrt(j) => " + (int) Math.sqrt(j));
+                for (int k = 2; k <= (int) Math.sqrt(j); k++) {
+                    if (j % k == 0) {
+                        jCount++;
+                        break;
+                    }
+                }
+
+                for (int k = 2; k <= (int) Math.sqrt(result); k++) {
+                    if (result % k == 0) {
+                        rCount++;
+                        break;
+                    }
+                }
+
+                if (jCount == 0 && rCount == 0) {
+                    num1 = j;
+                    num2 = result;
+                }
 
             }
 
-            // 시간 제한 난거
-            // 소수 리스트
-//            ArrayList<Integer> list = new ArrayList<>();
-//
-//            for (int j = 2; j <= num; j++) {
-//                int count = 0;
-//                for (int k = 2; k <= j; k++) {
-//                    if (j % k == 0) {
-//                        count++;
-//                    }
-//                }
-//                if (count == 1) {
-//                    list.add(j);
-//                }
-//            }
-//
-//            int prevGap = 0;
-//            int[] result = new int[2];
-//
-//            for (int j = 0; j < list.size(); j++) {
-//                int cal = num - list.get(j);
-//                if (list.contains(cal) && list.get(j) <= cal) {
-//
-//                    int currentGap = Math.abs(cal - list.get(j));
-//                    result[0] = list.get(j);
-//                    result[1] = cal;
-//
-//                    if (currentGap <= prevGap) {
-//                        break;
-//                    } else {
-//                        prevGap = currentGap;
-//                    }
-//                }
-//            }
-//
-//            System.out.println(result[0] + " " + result[1]);
+            sb.append(num1).append(" ").append(num2).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
