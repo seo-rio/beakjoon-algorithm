a2c20274da9c507f9635f9bafcf8f6e2e614f5c7
seo-rio <scs9310@gmail.com>
2023-03-30 22:25:16 +0900

    브루트포스 체스판 칠하기 해결

--- src/level/bruteforce/PaintChessBoard.java
+++ src/level/bruteforce/PaintChessBoard.java
@@ -3,6 +3,10 @@ package level.bruteforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
@@ -19,86 +23,76 @@ public class PaintChessBoard {
 
         String[] chessBoard = new String[n];
 
-        String key = "";
-        String revereKey = "";
-
-        int count = 0;
-        int reversCount = 0;
-
         for (int i = 0; i < n; i++) {
             stk = new StringTokenizer(br.readLine());
             chessBoard[i] = stk.nextToken();
-            if (i == 0) {
-                key = String.valueOf(chessBoard[i].charAt(0));
-                revereKey = String.valueOf(chessBoard[i].charAt(m - 1));
-            } else {
-                key = key.equals("W") ? "B" : "W";
-                revereKey = revereKey.equals("W") ? "B" : "W";
-
-            }
-
-//            String normal = "";
-            System.out.println("Key => " + key);
-            System.out.println("revereKey => " + revereKey);
-
-            for (int j = 0; j < m; j++) {
-                String color = String.valueOf(chessBoard[i].charAt(j));
-                System.out.println("origin" + j % 2 + " === color : " + color);
-                if (key.equals("W")) {
-                    if (j % 2 == 0) {
-                        if (!color.equals("W")) {
-                            count++;
-                        }
-                    } else {
-                        if (!color.equals("B")) {
-                            count++;
-                        }
-                    }
-                } else if (key.equals("B")) {
-                    if (j % 2 == 0) {
-                        if (!color.equals("B")) {
-                            count++;
-                        }
-                    } else {
-                        if (!color.equals("W")) {
-                            count++;
-                        }
-                    }
-                }
-            }
-
-            for (int j = m - 1; j >= 0; j--) {
-                String color = String.valueOf(chessBoard[i].charAt(j));
-                System.out.println("revers " + j % 2 + " === color : " + color);
-                if (revereKey.equals("W")) {
-                    if (j % 2 == 0) {
-                        if (!color.equals("W")) {
-                            reversCount++;
-                        }
-                    } else {
-                        if (!color.equals("B")) {
-                            reversCount++;
-                        }
-                    }
-                } else if (revereKey.equals("B")) {
-                    if (j % 2 == 0) {
-                        if (!color.equals("B")) {
-                            reversCount++;
-                        }
-                    } else {
-                        if (!color.equals("W")) {
-                            reversCount++;
-                        }
-                    }
-                }
-            }
-
-            System.out.println("Middle count => " + count);
-            System.out.println("Middle reversCount count => " + reversCount);
-
         }
 
-        System.out.println("count => " + count);
+        int totalCount = -1;
+
+        for (int i = 0; i <= n - 8; i++) {
+            for (int j = 0; j <= m - 8; j++) {
+                char key = 0;
+                int count = 0;
+                int reversCount = 0;
+                for (int k = 0; k < 8; k++) {
+                    char[] row = chessBoard[i + k].substring(j, j + 8).toCharArray();
+                    key = key == 0 ? row[0] : key == 'B' ? 'W' : 'B';
+                    if (key == 'B') {
+                        for (int z = 0; z < row.length; z++) {
+                            if (z % 2 == 0) {
+                                if ('B' != row[z]) {
+                                    count++;
+                                }
+                            } else {
+                                if ('W' != row[z]) {
+                                    count++;
+                                }
+                            }
+
+                            if (z % 2 != 0) {
+                                if ('B' != row[z]) {
+                                    reversCount++;
+                                }
+                            } else {
+                                if ('W' != row[z]) {
+                                    reversCount++;
+                                }
+                            }
+                        }
+                    } else {
+                        for (int z = 0; z < row.length; z++) {
+                            if (z % 2 == 0) {
+                                if ('W' != row[z]) {
+                                    count++;
+                                }
+                            } else {
+                                if ('B' != row[z]) {
+                                    count++;
+                                }
+                            }
+
+                            if (z % 2 != 0) {
+                                if ('W' != row[z]) {
+                                    reversCount++;
+                                }
+                            } else {
+                                if ('B' != row[z]) {
+                                    reversCount++;
+                                }
+                            }
+                        }
+                    }
+
+                }
+                if (totalCount == -1) {
+                    totalCount = Math.min(count, reversCount);
+                } else if (totalCount > Math.min(count, reversCount)) {
+                    totalCount = Math.min(count, reversCount);
+                }
+            }
+        }
+        System.out.println(totalCount);
     }
 
 
